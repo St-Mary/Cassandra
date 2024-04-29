45088a8bd7ab3566dddc9b167e088e5c413a9c3d
Noelle-Ai <gerardelliot49@gmail.com>
2024-04-29 04:31:57 +0200

    Add base of settings page

--- src/main/java/com/stmarygate/cassandra/game/GameApplication.java
+++ src/main/java/com/stmarygate/cassandra/game/GameApplication.java
@@ -8,40 +8,73 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import lombok.Getter;
 
 import java.awt.*;
 
 public class GameApplication extends Application {
 
+  @Getter
+  private static Stage primaryStage;
+
   public static void main(String[] args) {
     launch(args);
   }
+
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage stage) throws Exception {
+    primaryStage = stage;
     DatabaseManager.initialize();
-    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fxml/Main.fxml"));
-    primaryStage.setTitle("Saint Mary's Gate");
-    Scene scene = new Scene(root);
-    Font.loadFont(getClass().getClassLoader().getResource("fonts/retrogaming.ttf").toExternalForm(),
-            30);
-    scene.getStylesheets().add(getClass().getClassLoader().getResource("css/Main.css").toExternalForm());
-    primaryStage.setScene(scene);
+
     primaryStage.setOnCloseRequest(event -> System.exit(0));
-
     primaryStage.setResizable(false);
+    primaryStage.getIcons().add(new Image(
+            GameApplication.class.getClassLoader().getResource("img/icon.png").toString()));
 
-    primaryStage.getIcons().add(new Image(getClass().getClassLoader().getResource("img/icon.png").toString()));
+    loadFonts();
+    showMainPage();
+
     if (Taskbar.isTaskbarSupported()) {
       Taskbar taskbar = Taskbar.getTaskbar();
-
       if (taskbar.isSupported(Taskbar.Feature.ICON_IMAGE)) {
         Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
-        java.awt.Image dockIcon = defaultToolkit.getImage(getClass().getResource("/img/icon.png"));
+        java.awt.Image dockIcon = defaultToolkit.getImage(GameApplication.class.getResource("/img/icon.png"));
         taskbar.setIconImage(dockIcon);
       }
-
     }
 
     primaryStage.show();
+  }
+
+  public static void loadFonts() {
+    Font.loadFont(GameApplication.class.getClassLoader().getResource("fonts/retrogaming.ttf").toExternalForm(),
+            30);
+  }
+
+  public static void showSettingsPage() {
+    try {
+      Parent root = FXMLLoader.load(GameApplication.class.getClassLoader().getResource("fxml/Settings.fxml"));
+      Scene scene = new Scene(root, 1060, 600);
+      scene.getStylesheets().add(GameApplication.class.getClassLoader().getResource("css/Main" +
+              ".css").toExternalForm());
+      primaryStage.setScene(scene);
+      primaryStage.setTitle("Saint Mary's Gate - Settings");
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void showMainPage() {
+    try {
+      Parent root = FXMLLoader.load(GameApplication.class.getClassLoader().getResource("fxml/Main.fxml"));
+      Scene scene = new Scene(root, 1060, 600);
+      scene.getStylesheets().add(GameApplication.class.getClassLoader().getResource("css/Main.css").toExternalForm());
+      primaryStage.setScene(scene);
+      primaryStage.setTitle("Saint Mary's Gate");
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
--- src/main/java/com/stmarygate/cassandra/game/GameApplicationController.java
+++ src/main/java/com/stmarygate/cassandra/game/GameApplicationController.java
@@ -1,11 +1,30 @@
 package com.stmarygate.cassandra.game;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class GameApplicationController {
 
   public void handleLeaveBtn() {
     System.exit(0);
+  }
+
+  public void handleSettingsBtn() throws IOException {
+    GameApplication.showSettingsPage();
+    GameApplication.getPrimaryStage().show();
+  }
+
+  public void handleSaveBtn(MouseEvent mouseEvent) {
+  }
+
+  public void handleCancelBtn(MouseEvent mouseEvent) {
+    GameApplication.showMainPage();
+    GameApplication.getPrimaryStage().show();
   }
 }
